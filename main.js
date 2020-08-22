34771a0e756cb93bb08f1e9319193fbcef3bec21
Evgeniy <you@example.com>
2020-08-22 16:21:31 +0300

    create game and homework for this game

--- main.js
+++ main.js
@@ -1,44 +1,69 @@
-function formattedPhone(phone) {
-    if (lengthOfPhonenumber(phone) == 12){
-        let result = '';
-        for (let i = 0; i < phone.length; i++){
-            if(i == 0) {
-                result += addChar('', phone.charAt(i))
-            }else if (i == 1) {
-                result += addChar('', phone.charAt(i))
-            }else if (i == 2) {
-                result += addChar(' (', phone.charAt(i))
-            }else if (i == 3) {
-                result += addChar('', phone.charAt(i))
-            }else if (i == 4) {
-                result += addChar('', phone.charAt(i))
-            }else if (i == 5) {
-                result += addChar(') ', phone.charAt(i))
-            }else if (i == 6) {
-                result += addChar('', phone.charAt(i))
-            }else if (i == 7) {
-                result += addChar('', phone.charAt(i))
-            }else if (i == 8) {
-                result += addChar('-', phone.charAt(i))
-            }else if (i == 9) {
-                result += addChar('', phone.charAt(i))
-            }else if (i == 10) {
-                result += addChar('-', phone.charAt(i))
-            }else if (i == 11) {
-                result += addChar('', phone.charAt(i))
-            }
-        }
-        return result;
+const btn = document.getElementById('btn-kick');
+const btn2 = document.getElementById('btn-kick2')
+const character = {
+    name: "Pikachu",
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressBar: document.getElementById('progressbar-character')
+}
+
+const enemy = {
+    name: "Charmander",
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressBar: document.getElementById('progressbar-enemy')
+}
+
+btn.addEventListener('click', () => {
+    handlerClik();
+});
+btn2.addEventListener('click', () => {
+    handlerClik(2);
+})
+
+function handlerClik(num = 1) {
+    changeHP(random() * num, character);
+    changeHP(random() * num, enemy);
+}
+
+function init() {
+    console.log('init game');
+    renderHP(character);
+    renderHP(enemy);
+}
+
+function renderHP (person) {
+    renderHPlife(person);
+    renderProgressBarHP(person);
+}
+
+function renderHPlife(person) {
+    // const character = document.getElementById('health-character');
+    // console.log(character.innerText);
+    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+}
+
+function renderProgressBarHP(person) {
+    person.elProgressBar.style.width = person.damageHP + '%';
+}
+
+function changeHP (count, person) {
+    if(person.damageHP < count){
+        person.damageHP = 0;
+        alert(' Персонаж ' + person.name + ' проиграл.');
+        btn.disabled = true;
+        btn2.disabled = true;
     } else {
-        return "number of phone is not has 12 characters";
+        person.damageHP -= count;
     }
+    
+    renderHP(person);
+} 
+
+function random() {
+    return Math.ceil(Math.random() * 20)
 }
-function addChar(char, resultChar) {
-    return char + resultChar ;
-}
-function lengthOfPhonenumber(str){
-    return str.length
-}
-console.log(formattedPhone('+71234567890'));
-console.log(formattedPhone('+712345678901111'));
-console.log(formattedPhone('+71237890'));
+
+init();
